a2774a7f160400457707d500e1948314cf8d888f
trevor <trgr2496@colorado.edu>
2019-09-16 11:25:49 -0600

    Started mucking around

---
+++ img/pixelart/tiledmaps/32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="32" tilewidth="32" tileheight="32" tilecount="248" columns="8">
+ <image source="../../../../Users/trevorgrant/Documents/Pixel_Proj/32_pixel_ground.png" width="256" height="992"/>
+</tileset>
---
+++ img/pixelart/tiledmaps/32_pixel_ground_Sorted.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="32_pixel_ground_Sorted" tilewidth="32" tileheight="32" tilecount="870" columns="29">
+ <image source="../../../ZIP/New_Pixel_Town/32_pixel_ground_Sorted.png" width="928" height="960"/>
+</tileset>
